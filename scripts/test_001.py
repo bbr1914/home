aa78f737c3321f01980efb0f94f57d30a90378e7
bbr1914 <595429054@qq.com>
2020-07-07 00:03:44 +0800

    Initial commit

---
+++ scripts/test_001.py
@@ -0,0 +1,19 @@
+import pytest
+
+class Test_ABC:
+
+    def setup_class(self):
+        print(">>>>>>>setup_class")
+        print(12222222222222222222222)
+    def teardown_class(self):
+        print(">>>>>>>teardown_class")
+    @pytest.mark.run(order=-1)
+    def test_a(self):
+        print(">>>>>>test_a")
+        assert False
+    def test_b(self):
+        print(">>>>>>>test_b")
+        assert True
+
+if __name__ == '__main__':
+    pytest.main("-s test_001.py")
